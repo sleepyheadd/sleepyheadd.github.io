fa42624f8842eeb46a1a35689b812434a0987514
sleepyheadd <92340125+sleepyheadd@users.noreply.github.com>
2022-11-22 13:09:38 -0500

    js file added!

---
+++ script.js
@@ -0,0 +1,37 @@
+var x = 0;
+
+function changeImageUp() {
+    x++;
+    changeImage();
+}
+
+function changeImageDown() {
+    x--;
+    changeImage();
+}
+
+function changeImage() {
+    var image = document.getElementById("imageScroll");
+    var numImage = 4;
+
+    if (x > numImage-1) {
+        x = 0;
+    } else if (x < 0) {
+        x = numImage-1;
+    }
+
+    switch (x) {
+        case 0:
+            image.src = "Images/catboyyy.png";
+            break;
+        case 1:
+            image.src = "Images/scrimbloshirt.png";
+            break;
+        case 2:
+            image.src = "Images/treeboy.png";
+            break;
+        case 3:
+            image.src = "Images/wormdevoursconneticut.png"
+            break;
+    }
+}
